d6fa79f1fb44b38c67f2931acee94e5a4117ae6b
Franchesca2002 <148365325+Franchesca2002@users.noreply.github.com>
2024-06-07 07:45:15 -0500

    Add files via upload

---
+++ evaluacion.java
@@ -0,0 +1,67 @@
+package evaluacionCristian;
+
+import java.util.ArrayList;
+
+public class evaluacion {
+
+	public static void main(String[] args) {
+		ArrayList producto = new ArrayList< Strings>(); //se crea un objeto tipo scanner para leer
+				
+				
+		        
+		        //se imprime los datos ingresados
+		        System.out.println("producto:" + producto);
+		        
+		        //se eliminara un producto del inventario
+		        System.out.print(producto)remove(1);
+		        
+		        //mostrar los productos del inventario y su cantidad
+		        
+		        ArrayList<String> listaProductos=new ArrayList<String>();
+		        
+		        listaProductos.add("zapatos $ 20");
+		        listaProductos.add("chaquetas $ 30");
+		        listaProductos.add("pantalon $ 45");
+		        listaProductos.add("vestidos $ 35");
+		        listaProductos.add("camisa $ 15");
+		        
+		        
+		        System.out.println();
+		        System.out.println(listaProductos);
+		        
+		      //menu principal
+		        System.out.print(listaProductos);
+		        
+		        menu principal opcion=("1 zapatos");
+                menu principal opcion=("2 chaquetas");
+                menu principal opcion=("3 pantalon");
+                menu principal opcion=("4 vestidos");
+                menu principal opcion=("5 camisa");
+                menu principal opcion=("6 salir");
+                
+                  
+              //se solicita agregar un nuevo producto
+				System.out.print("ingrese producto nuevo" );
+		        String producto = listaProductos.String();
+		        
+		        //cosultar un producto por el nombre
+		        System.out.print("ingrese nombre de producto");
+		        String nombre = nombre.String();
+		        
+		       //eliminar un producto
+                System.out.print(producto);
+                remove(1);
+                System.out.print(listaProductos)remove(1);
+                
+                //mostrar lista de productos
+                System.out.println(listaProductos);
+                
+               
+           private static void remove(int i) {
+		System.out.println("\n<<<<listaProductos>>>>")remove(1);
+		
+		
+		
+	}
+
+}
